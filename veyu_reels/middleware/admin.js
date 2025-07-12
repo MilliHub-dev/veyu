455a4f737f0fdeaff0606685266dc7c33a9eabeb
Dr milli <100302736+drmilli@users.noreply.github.com>
2025-07-12 14:41:36 +0100

    removed reels

--- veyu_reels/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const adminOnly = (req, res, next) => {
-  if (!req.user?.is_admin) {
-    return res.status(403).json({ message: "Admin access required" });
-  }
-  next();
-};
-
-export default adminOnly;
